1e05ac25ceca53d393e6cb73eb6f89b3be9b2917
Johan <johan@macair2020>
2020-05-25 22:34:43 +0200

    fix tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,7 @@ describe('Time-based OTP', () => {
   const l = 20
   const secret  = I.generateSecret(l);
   test('secret generation', () => {
-    expect(secret.length).toEqual(l)
+    expect(secret.length).toEqual(32)
   })
 
   const token = I.generateTOTP(secret);

321c3881f674386461547965875b6429e4c2825c
Johan <johan@macair2020>
2020-11-14 22:35:50 +0100

    add QR string

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,25 +1,35 @@
-import * as I from './index';
-describe('Time-based OTP', () => {
-  const l = 20
-  const secret  = I.generateSecret(l);
-  test('secret generation', () => {
-    expect(secret.length).toEqual(32)
-  })
+import * as I from "./index";
+describe("Time-based OTP", () => {
+  const l = 20;
+  const secret = I.generateSecret(l);
+  test("secret generation", () => {
+    expect(secret.length).toEqual(32);
+  });
 
   const token = I.generateTOTP(secret);
 
-  test('verify token', () => {
-    expect(I.verifyTOTP(token, secret)).toEqual(true)
-  })
+  test("verify token", () => {
+    expect(I.verifyTOTP(token, secret)).toEqual(true);
+  });
 });
 
-/*
-this is a test to see if the integration actually wors with the google auth
-test('manual testing', () => {
-  //const secret  = I.generateSecret();
-  //console.log(secret)
-  const secret = 'YFA3UTGAGUPJEFQYNNR5VKBFU5GQ6H6O';
-  const t = 412587//92300
+test("to QR string", () => {
+  const s =
+    "otpauth://totp/myname?secret=asecret&algorithm=SHA256&digits=6&period=15";
+  expect(I.toQRString("myname", "asecret")).toEqual(s);
+});
 
-  expect(I.verifyTOTP(t, secret)).toEqual(true)
-})*/
+//this is a test to see if the integration actually wors with the google auth
+// note that the key "seems to be" valid for two cycles
+/*
+test("manual testing", () => {
+  //const secret = I.generateSecret();
+
+  const secret = "WENBV6U26A7HXMVBRXCIBU22JTLGUFRM"; // "YFA3UTGAGUPJEFQYNNR5VKBFU5GQ6H6O";
+
+  console.log(I.toQRString("test", secret));
+  const t = 764975;
+
+  expect(I.verifyTOTP(t, secret)).toEqual(true);
+});
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-import * as Crypto from 'crypto';
-import Base32 from 'hi-base32';
+import * as Crypto from "crypto";
+import Base32 from "hi-base32";
 
-export const generateSecret = (length: number = 20):string => {
+export const generateSecret = (length: number = 20): string => {
   const randomBuffer = Crypto.randomBytes(length);
-  return Base32.encode(randomBuffer).replace(/=/g, '')
-}
+  return Base32.encode(randomBuffer).replace(/=/g, "");
+};
 
-export const decodeSecret = (secret: string) => Base32.decode.asBytes(secret)
+export const decodeSecret = (secret: string) => Base32.decode.asBytes(secret);
 
-export const generateHOTP = (secret: string, counter: number):number => {
-  const decodedSecret = decodeSecret(secret)
+export const generateHOTP = (secret: string, counter: number): number => {
+  const decodedSecret = decodeSecret(secret);
   const buffer = Buffer.alloc(8);
 
-  for (let i= 0; i < 8; i++) {
-    buffer[7-i] = counter & 0xff;
+  for (let i = 0; i < 8; i++) {
+    buffer[7 - i] = counter & 0xff;
     counter = counter >> 8;
   }
 
-  // 
-  const hmac = Crypto.createHmac('sha1', Buffer.from(decodedSecret))
+  //
+  const hmac = Crypto.createHmac("sha1", Buffer.from(decodedSecret));
   hmac.update(buffer);
-  const hmacResult:Buffer = hmac.digest()
+  const hmacResult: Buffer = hmac.digest();
 
-  const code = dynamicTruncationFn(hmacResult)
+  const code = dynamicTruncationFn(hmacResult);
 
   return code % 10 ** 6;
-}
+};
 
-const dynamicTruncationFn = (hmacValue:Buffer):number => {
+const dynamicTruncationFn = (hmacValue: Buffer): number => {
   const offset = hmacValue[hmacValue.length - 1] & 0xf;
 
   return (
@@ -35,21 +35,44 @@ const dynamicTruncationFn = (hmacValue:Buffer):number => {
     ((hmacValue[offset + 1] & 0xff) << 16) |
     ((hmacValue[offset + 2] & 0xff) << 8) |
     (hmacValue[offset + 3] & 0xff)
-  )
-}
+  );
+};
 
-export const generateTOTP = (secret:string, window = 0):number => {
+export const generateTOTP = (secret: string, window = 0): number => {
   const counter = Math.floor(Date.now() / 30000);
   return generateHOTP(secret, counter + window);
-}
+};
 
-export const verifyTOTP = (token: number, secret: string, window: number = 1):boolean => {
-  if(Math.abs(+window) > 10) {
-    throw Error('window size is too large');
+// https://freeotp.github.io/qrcode.html
+export const toQRString = (
+  name: string,
+  secret: string,
+  algorithm: "SHA1" | "SHA224" | "SHA256" | "SHA384" | "SHA512" = "SHA256",
+  digits: number = 6,
+  period: number = 15
+) => {
+  const t = { secret, algorithm, digits, period };
+
+  const s: string = Object.entries(t)
+    .map(([k, v]) => {
+      return `${k}=${encodeURIComponent(v)}`;
+    })
+    .join("&");
+
+  return "otpauth://totp/" + name + "?" + s;
+};
+
+export const verifyTOTP = (
+  token: number,
+  secret: string,
+  window: number = 1
+): boolean => {
+  if (Math.abs(+window) > 10) {
+    throw Error("window size is too large");
   }
 
   for (let errorWindow = -window; errorWindow <= +window; errorWindow++) {
-    const totp:number = generateTOTP(secret, errorWindow)
+    const totp: number = generateTOTP(secret, errorWindow);
 
     if (token === totp) {
       return true;
@@ -57,4 +80,4 @@ export const verifyTOTP = (token: number, secret: string, window: number = 1):bo
   }
 
   return false;
-}
+};

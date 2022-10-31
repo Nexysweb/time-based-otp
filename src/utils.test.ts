1d8759cdc816241d7b5f7de9fc455fa3ee258654
John B <johan@nexys.ch>
2022-10-31 22:15:42 +0100

    Create utils.test.ts

---
+++ src/utils.test.ts
@@ -0,0 +1,9 @@
+import * as U from "./utils";
+
+test("secretFromUrl", () => {
+  const url = "otpauth://totp/Digis?secret=JTYH5G4IZPVMSQK3CVF5DH6IT3HDV3NI&algorithm=SHA256&digits=6&period=1";
+  const secret = "JTYH5G4IZPVMSQK3CVF5DH6IT3HDV3NI";
+
+  expect(secretFromUrl(url)).toEqual(secret);
+});
+  
